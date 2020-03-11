211501023b2b31e7a0e6bcd0bc51c2a84c14f866
Leeland Miller <leelandmiller@gmail.com>
2020-03-11 14:50:12 -0700

    fix warning in _document

--- pages/_document.js
+++ pages/_document.js
@@ -22,7 +22,7 @@ export default class MyDocument extends Document {
         </Head>
         <body>
           <div id="fb-root"></div>
-          <script async defer crossorigin="anonymous"
+          <script async defer crossOrigin="anonymous"
             src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v5.0&appId=527762481420216&autoLogAppEvents=1">
           </script>
           <Main />
